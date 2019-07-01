601cf31a1c416dfbe854a575a7ac192c01ee1c91
Julia Bertsch <jbertsch2002@gmail.com>
2019-07-01 16:07:18 +0200

    Update Evo project
    dots.js
    - added checkBoudary with dead condition (if hits wall = dead)
    
    ecosystem.js
    - added middle as global variable
    
    >> must fix - direction applies to all dots. see issue #16

--- Evo project/dot.js
+++ Evo project/dot.js
@@ -1,27 +1,45 @@
 function Dot(startpos) {
     this.pos = startpos;
-    this.vel = createVector();
-    this.acc = createVector();
+    this.vel = createVector(0,0);
+    this.acc = createVector(0,0);
 
-    this.maxspeed = 10;                     // #13
-    this.size = 12;                         // #14
+    this.maxspeed = 2;                     // #13
+    this.size = 20;                        // #14
+    this.dead = false;
 
+    //this.dir = p5.Vector.random2D();
+    this.dir = p5.Vector.fromAngle(random() * 360);
     
     this.run = function() {
         this.show();
+        this.checkBoundary();
+        this.applyForce(this.dir);
+        this.update();
     }
 
     this.update = function() {
-        this.vel.add(this.acc);
-        this.vel.limit(this.maxspeed);      // #13
-        this.pos.add(this.vel);
-        this.acc.mult(0); 
+        if (!this.dead) {
+            this.vel.add(this.acc);
+            this.vel.limit(this.maxspeed);      // #13
+            this.pos.add(this.vel);
+            this.acc.mult(0);
+        } 
+    }
+
+    this.checkBoundary = function() {
+        if (this.pos.x < 1 || this.pos.x > window_width-1 || this.pos.y < 1 || this.pos.y > window_height-1) {
+            this.dead = true;
+        }
     }
 
     this.show = function() {
-        fill(0, 100);
+        fill(2, 100);
         noStroke();
         ellipse(this.pos.x, this.pos.y, this.size, this.size);
+    }
+
+    this.applyForce = function(force) {
+        this.acc.add(force);
     }
 
 }
--- Evo project/ecosystem.js
+++ Evo project/ecosystem.js
@@ -2,15 +2,23 @@
 var window_width = 600;
 var window_height = 600;
 
-let dot;
+let middle;
+let dots;
+var popsize = 2;
 
 function setup() {
+    middle = createVector(window_width/2, window_height/2);     // #12
     createCanvas(window_width, window_height);
-    var middle = createVector(window_width/2, window_height/2);     // #12
-    dot = new Dot(middle);
+    dots = new Array(popsize);
+    for (let i = 0; i < popsize; i++) {
+        dots[i] = new Dot(middle);
+    }
+
 }
 
 function draw() {
     background(200);
-    dot.run();
+    for (let i = 0; i < popsize; i++) {
+        dots[i].run();
+    }
 }
